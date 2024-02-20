3ea14fc904e21d3484191b8137976d868123b00f
Dawit Tefera <69167983+tevBoyz@users.noreply.github.com>
2024-02-20 10:58:24 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,176 @@
+// var mytable = document.getElementById("list_for_guessed");
+var mytable = document.getElementById("table_history").getElementsByTagName('tbody')[0];
+var mydisplay = document.getElementById("num_display");
+var correct_nums = document.getElementById("correct_nums");
+var correct_pos = document.getElementById("correct_pos");
+var trials = document.getElementById("tries");
+var curr_num;
+var res = [-1, -1, -1];
+var guessed = []
+
+
+var num_to_Guess = random4Digit();
+// var num_to_Guess = 2134;
+
+var input = document.getElementById("input");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    check();
+  }
+});
+
+
+function addRow(number, cn, cp){
+    guessed.push(number);
+    // const text = guessed.length +". " + number + " - Correct Numbers: " + cn + " - Correct Positions: " + cp;
+    var new_content = "<tr> <td>"+ guessed.length +"</td> <td>"+ number +"</td> <td>"+ cn +"</td> <td>"+ cp + "</td> </tr>";
+    var new_row = mytable.insertRow(0);
+    new_row.innerHTML = new_content;
+    //change(text);
+    trials.innerHTML = guessed.length;
+}
+
+// function change(txt) {
+//     var x=document.getElementById("list_for_guessed");
+//     newLI = document.createElementNS(null,"li");
+//     newText = document.createTextNode(txt);
+//     newLI.appendChild(newText);
+//     x.appendChild(newLI);
+//     }
+
+function displayOnDisplay(num, cn, cp){
+    mydisplay.innerHTML = (num);
+    correct_nums.innerHTML =  cn;
+    correct_pos.innerHTML =  cp;
+}
+
+function compare(guessed_num){
+    res = [0, 4, 4];
+    if(guessed_num == num_to_Guess){
+        res[0] = 1;
+    }
+    else{
+        let g = String(guessed_num).split('').map(Number);
+        let an = String(num_to_Guess).split('').map(Number);
+
+        res[0] = 0;
+        res[1] = getCorrectPosCount(g, an);
+        res[2] = getRightNums(g, an);
+    }
+}
+
+
+function check(){
+    console.log(num_to_Guess);
+    curr_num = document.getElementById("input").value;
+    if (curr_num >= 1000 && curr_num <= 9999){
+        document.getElementById("input").classList.remove("red")
+        compare(curr_num);
+        if (res[0] == 0){
+            displayOnDisplay(curr_num, res[2], res[1]);
+            addRow(curr_num, res[2], res[1]);
+            curr_num = document.getElementById("input").value = "";
+        document.getElementById("input").focus();
+        }
+        else{
+            displayOnDisplay(curr_num, res[2], res[1]);
+            addRow(curr_num, res[2], res[1]);
+            document.getElementById("main").style.display = "none";
+            document.getElementById("congrats").style.display = "flex";
+            document.getElementById("corres").innerHTML = curr_num;
+        }
+    }
+    else{
+        alert('Please enter exactly 4 digits. No Zeroes, No repititions');
+        curr_num = document.getElementById("new_number").value = "";
+        document.getElementById("new_number").focus();
+    }
+}
+
+function getCorrectPosCount(guessed, answer){
+    let count = 0;
+    for(let i = 0; i < 4; i++){
+        if(guessed[i] == answer[i])
+            count++;
+    }
+    return count;
+}
+
+
+function back(){
+    document.getElementById("main").style.display = "flex";
+    document.getElementById("congrats").style.display = "none";
+}
+
+function restart(){
+    location.reload();
+}
+
+
+function getRightNums(guessed, answer){
+    let count = 0;
+    let gotNums = [];
+    for(let i = 0; i < 4; i++){
+        if ((answer.indexOf(guessed[i]) !== -1) && (gotNums.indexOf(guessed[i]) == -1))
+        { 
+            count++;
+            gotNums.push(guessed[i]);
+        }
+    }
+    return count;
+}
+
+
+function validateInput() {
+    const input = document.getElementById('input');
+    const value = input.value;
+    let c = String(value).split('').map(Number);
+
+    if (value.length > 4 || isNaN(value)) {
+      alert('Please enter exactly 4 digits. No Zeroes, No repititions');
+      input.value = '';
+    }
+    if(c.indexOf(0) !== -1){
+    alert('No Zeroes, please');
+    input.value = '';
+    }
+    if(hasDuplicates(value)){
+        alert('No Duplicates, please');
+        input.value = ''; 
+    }
+  }
+
+  function info(){
+    if(document.getElementById('info').style.display == 'none'){
+        document.getElementById('info').style.display = 'block';
+        document.getElementById('infobutton').style.backgroundColor = 'white';
+        document.getElementById('infobutton').style.color = 'rgba(249, 93, 71, 0.75)';
+        document.getElementById('main_container').style.display = 'none';
+    }
+    else{
+        document.getElementById('info').style.display = 'none';
+        document.getElementById('infobutton').style.backgroundColor = 'rgba(249, 93, 71, 0.75)';
+        document.getElementById('infobutton').style.color = 'white'
+        document.getElementById('main_container').style.display = 'flex';
+    }
+  }
+
+  function hasDuplicates(a) {
+
+    const noDups = new Set(a);
+  
+    return a.length !== noDups.size;
+  }
+
+  function random4Digit(){
+    return shuffle( "123456789".split('') ).join('').substring(0,4);
+  }
+  
+  function shuffle(o){
+      for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+      return o;
+  }
+  
